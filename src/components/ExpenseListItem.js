41df27275738865714d945be990150f9d0b0bbaf
reit85 <reit85@gmail.com>
2018-03-02 18:17:45 +0100

    setup formattion for amount and createdAt showing

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment'
+import numeral from 'numeral'
+
+//moment.locale('de'); // Problem mit WEBPACK lässt denn Test nicht zu ...
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
-    <p>{amount} - {createdAt}</p>
+    <p>
+       {(amount/100).toLocaleString('de-DE', {style: 'currency', currency: 'EUR'})}
+       - 
+      {moment(createdAt).format('DD.MM.YYYY')}
+     </p>
   </div>
 );
 
